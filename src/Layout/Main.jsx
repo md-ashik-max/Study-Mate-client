3adf58c4f44b326de96d4574ef7e92748cd70995
md-ashik-max <ar1214697@gmail.com>
2024-05-09 23:17:33 +0600

    added basic routing setup and navbar

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Main = () => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import { NavLink } from "react-router-dom";
+
+
+const Navbar = () => {
+
+    const links = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/assignments">Assignments</NavLink></li>
+        <li><NavLink to="/assignments">Register</NavLink></li>
+
+    </>
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        {links}
+                    </ul>
+                </div>
+                <div className="flex md:flex-col items-center">
+                    <img className="h-16 w-16" src="https://i.ibb.co/TgMVs48/student.png" alt="" />
+                    <h3 className="text-2xl font-bold">Study Mate</h3>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {links}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                {/* The button to open modal */}
+                <label htmlFor="my_modal_7" className="btn btn-outline btn-info">Login</label>
+
+                {/* Put this part before </body> tag */}
+                <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+                <div className="modal" role="dialog">
+                    <div className="modal-box w-11/12 max-w-4xl">
+                        
+                    </div>
+                    <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
