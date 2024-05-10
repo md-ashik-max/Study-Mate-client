7e6a2f8243260e0ae41d5529d5b3743462acf9dc
md-ashik-max <ar1214697@gmail.com>
2024-05-10 20:41:59 +0600

    added authProvider and createUser function implement on register page

--- src/Pages/Home/Register/Register.jsx
+++ src/Pages/Home/Register/Register.jsx
@@ -1,7 +1,61 @@
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { AiFillEyeInvisible } from "react-icons/ai";
+import { IoEyeSharp } from "react-icons/io5";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 
 const Register = () => {
+    const { createUser} = useContext(AuthContext);
+    const [registerError, setRegisterError] = useState('');
+    const [showPassword,setShowPassword]=useState(false);
+    const location = useLocation()
+    const navigate = useNavigate()
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        const email = data.email;
+        const password = data.password;
+        // const fullName = data.fullName;
+        // const image = data.image;
+
+        if (password.length < 6) {
+            setRegisterError('Password should be at least 6 characters or longer');
+            return;
+        }
+        else if (!/[A-Z]/.test (password)){
+            setRegisterError("Password must contain at least one uppercase letter."
+            );
+            return;
+
+        }
+        else if (!/[a-z]/.test (password)){
+            setRegisterError("Password must contain at least one lowercase letter."
+            );
+            return;
+
+        }
+
+        setRegisterError('')
+        createUser(email, password)
+            .then(() => {
+                Swal.fire({
+                    icon: "success",
+                    title: "Created User Successfully",
+                    showConfirmButton: false,
+                    timer: 1500
+                  });
+                navigate(location?.state ? location.state : "/")
+
+            })
+            .catch(error => {
+                setRegisterError(error.message)
+                // toast.error("Oops! Email Already Registered")
+            })
+
+    }
+
     return (
         <div className="flex flex-col my-12 md:flex-row justify-center items-center max-w-5xl mx-auto rounded-2xl shadow-2xl">
             <div className="animate__animated animate__fadeInRight w-full h-full py-6  md:py-[244px] flex flex-col items-center bg-[#512DA8] rounded-r-2xl rounded-t-3xl md:rounded-r-[150px] text-center text-white">
@@ -14,36 +68,45 @@ const Register = () => {
                     <h3 className="text-3xl font-bold">Register</h3>
                 </div>
 
-                <form className="card-body">
+                <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Name</span>
                         </label>
-                        <input type="text" placeholder="Name" className="input input-bordered" required />
+                        <input type="text" placeholder="Name" name="name" className="input input-bordered" {...register("fullName", { required: true })} />
+                        {errors.fullName && <span className="text-red-500">This field is required</span>}
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Photo URL</span>
                         </label>
-                        <input type="text" placeholder="Photo URL" className="input input-bordered" required />
+                        <input type="text" placeholder="Photo URL" name="photo" className="input input-bordered" {...register("image")} />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
+                        <input type="email" placeholder="Email" name="email" className="input input-bordered" {...register("email", { required: true })} />
+                        {errors.email && <span className="text-red-500">This field is required</span>}
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <div className="relative">
+                            {showPassword ? <span className="absolute right-2 top-4 text-xl" onClick={() => setShowPassword(!showPassword)}><IoEyeSharp /></span> : <span className="absolute right-2 top-4 text-xl" onClick={() => setShowPassword(!showPassword)}><AiFillEyeInvisible /></span>}
+                            <input type={showPassword ? "text" : "password"} placeholder="password" name="password" className="input input-bordered w-full" {...register("password", { required: true })} />
+                        </div>
+                        {errors.password && <span className="text-red-500">This field is required</span>}
                         <label className="label">
                             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                         </label>
                     </div>
                     <div className="form-control mt-6">
-                      <input className="btn bg-transparent text-emerald-300 border border-sky-400 hover:text-white  hover:bg-gradient-to-r from-emerald-300 to-sky-400" type="submit" value="Register" />
+                        <button className="btn bg-transparent text-emerald-300 border border-sky-400 hover:text-white  hover:bg-gradient-to-r from-emerald-300 to-sky-400">Register</button>
+                        {
+                            registerError && <p className="text-red-600">{registerError}</p>
+                        }
                     </div>
                 </form>
             </div>
